3bb241f93ecdd1ce657a12e93e5b6c5963c76d6a
Evelyn Stange <fabiansta@protonmail.com>
2023-11-23 23:49:34 +0100

    add zoom, ui and maingame scene connecting nodes

--- testing_area/prototype_testing/overworld/script/Player.gd
+++ testing_area/prototype_testing/overworld/script/Player.gd
@@ -1,14 +1,27 @@
 extends CharacterBody2D
 
 
-@export var SPEED = 1000.0
-
+@export var SPEED = 100
+@export var zoomlevel:Vector2 = Vector2(1,1)
 @onready var anim :  AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta):
 	player_movement(delta)
+	update_camera()
+
+func update_camera():
+	
+	if Input.is_action_pressed("zoom_in"):
+		if zoomlevel.x < 9:
+			zoomlevel = Vector2(zoomlevel.x+.1, zoomlevel.y+.1)
+	if Input.is_action_pressed("zoom_out"):
+		if zoomlevel.x > 1:
+			zoomlevel = Vector2(zoomlevel.x-.1, zoomlevel.y-.1)
+	$PlayerCamera.zoom = zoomlevel
 
 func player_movement(delta):
+	
+	
 	
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed("move_up"):
---
+++ testing_area/prototype_testing/overworld/ui/overworld_ui/overworld_ui.gd
@@ -0,0 +1,42 @@
+extends CanvasLayer
+
+class_name OverWorldUI
+
+@onready var health_label = $Control/MarginContainer/VBoxContainer/HBoxContainer/Health
+@onready var playername_label = $Control/MarginContainer/VBoxContainer/HBoxContainer2/PlayerName
+@onready var quest_label = $Control/MarginContainer/VBoxContainer/HBoxContainer3/VBoxContainer/QuestList
+
+var current_quests:Array = ["hello!","world?"]:
+	set(newArray):
+		# if a new array was given, we update its value, also the label
+		current_quests = newArray
+		_update_quest_label()
+
+var current_health:int = 100:
+	set(updatedHealth):
+		current_health = updatedHealth
+		_update_health_label()
+
+func _update_quest_label():
+	# iterate over item
+	var newString:String  = ""
+	for quest_string in current_quests:
+		newString += str(quest_string) + "\n"
+	quest_label.text = newString
+
+func _update_health_label(): 
+	health_label.text = str(current_health)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# initializing UI accordingly
+	_update_health_label()
+	# not sure whether we should call  and always set this here?
+	playername_label.text = "Eve"
+	
+	_update_quest_label()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
