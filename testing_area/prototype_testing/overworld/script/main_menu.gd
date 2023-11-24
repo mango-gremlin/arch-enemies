bffc4f857084a7cd77c79e268b252a6a7f8551e7
Paul Masan <me@paulmasan.com>
2023-11-24 01:07:33 +0100

    mocked up some dummy main menu

---
+++ testing_area/prototype_testing/overworld/script/main_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+
+func _on_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_pressed(): #quit
+	get_tree().quit()
+
+
+func _on_about_pressed(): #about
+	pass
