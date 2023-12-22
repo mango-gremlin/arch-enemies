2ed21266d3874624992ee96e8025ed93e0a4e5c3
Katja Bochniak <57258671+Kabooni@users.noreply.github.com>
2023-12-22 19:21:13 +0100

    polish player animations

--- Arch-enemies/bridges/script/player.gd
+++ Arch-enemies/bridges/script/player.gd
@@ -38,7 +38,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	direction = Input.get_vector("left", "right", "up", "down")
+	direction = Input.get_vector("left", "right", "ui_up", "ui_down")
 	if direction:
 		velocity.x = direction.x * speed
 	else:
