d8679fafd36e7770b22e2b68d4d4c4d21dbe2ad1
Adam König <adam.koenig@student.uni-tuebingen.de>
2023-12-25 22:52:10 +0100

    added comments

--- Arch-enemies/bridges/script/grid.gd
+++ Arch-enemies/bridges/script/grid.gd
@@ -1,10 +1,15 @@
 extends TileMap
 
+#We define the basical variables here
+
+#Like the Tile Size
 var tile_size = tile_set.tile_size
 
+#The Dimensions of the Grid
 var x_size = 115
 var y_size = 65
 
+#And finally some values we need later
 var grid_size = Vector2(x_size, y_size)
 var grid = []
 var start_grid = [[[]]]
@@ -12,22 +17,30 @@ var last_states = []
 var save_states = 10
 var state = 0
 
+#This defined the start-zone for the bridge
 var start_zone = [Vector2(11, 50), Vector2(12, 50), Vector2(13, 50), Vector2(14, 50)]
+
+#And the start zone for the Fox
 var fox_start = [Vector2(4, 50), Vector2(5, 50), Vector2(6, 50), Vector2(7, 50), Vector2(8, 50),
 				Vector2(4, 49), Vector2(5, 49), Vector2(6, 49), Vector2(7, 49), Vector2(8, 49), 
 				Vector2(4, 48), Vector2(5, 48), Vector2(6, 48), Vector2(7, 48), Vector2(8, 48),
 				Vector2(4, 47), Vector2(5, 47), Vector2(6, 47), Vector2(7, 47), Vector2(8, 47)]
 
+#This is the signal we use to transfer the current grid to child nodes
 signal current_grid(current_grid)
 
+#These are the different kind of object we can have in grid cells
 enum ENTITY_TYPES {GROUND, WATER, ANIMAL, FORBIDDEN, ALLOWED, AIR}
 
 func _ready():
+	#We save the previous states of the grid in an array, this array is initalized here
 	for i in range(save_states):
 		last_states.append([[]])
+	#Here we iterated over the grid and fill it
 	for x in range(grid_size.x):
 		grid.append([])
 		for y in range(grid_size.y):
+			#These are the specific fields we want to fill, essentially the level is defined here
 			if((x < 15 or (x < x_size and x > (x_size - 15))) and y > (y_size - 15)):
 				grid[x].append(ENTITY_TYPES.GROUND)
 			elif(Vector2(x, y) in start_zone):
@@ -38,14 +51,22 @@ func _ready():
 				grid[x].append(ENTITY_TYPES.WATER)
 			else:
 				grid[x].append(ENTITY_TYPES.AIR)
+	#The previous function only filled the internal represtation of grid
+	#With color Grid we fill the displayed grid with color
 	color_grid()
+	
+	#Now we save the inital state of the grid for reset and previous state
 	start_grid = grid.duplicate(true)
 	last_states[0] = grid.duplicate(true)
 	
 func color_grid():
+	#This function color the grid
+	#We iterate over all the cells
 	for x in range(grid_size.x):
 		for y in range(grid_size.y):
 			var square = grid[x][y]
+			#An fill them with the correct tile
+			#We ignore animals. Animals can have many different tiles and are handeled seperate
 			if (square != ENTITY_TYPES.ANIMAL):
 				match square:
 					ENTITY_TYPES.GROUND:
@@ -58,13 +79,18 @@ func color_grid():
 						set_cell(0, Vector2i(x, y), 0, Vector2i(1, 1))	
 
 func update_grid(pos, data):
+	#If we drag an animal onto a cell we update the grid here
 	var animal = data["animal"]
 	var x = pos.x
 	var y = pos.y
+	#Based on the animal we use the apprioate filling
 	match animal:
 		"DEER":
+			#The Deer has empty cells in its tiles, they need to be excempt
 			var empty = [Vector2i(0, 3), Vector2i(3, 0), Vector2i(3, 1), Vector2i(3, 2)]
+			#Similarly we need to define what new tiles are now ALLOWED for dragging
 			var new_allowed = [Vector2i(0, 3), Vector2i(1, 4), Vector2i(2, 4), Vector2i(3, 4)]
+			#These two loops just iterate over the grid cells we want to fill
 			for delta in range(4):
 				for epsilon in range(4):
 					var tile_pos = Vector2i(delta, epsilon) 
@@ -75,7 +101,9 @@ func update_grid(pos, data):
 				if(grid[x + position.x][y - position.y] == ENTITY_TYPES.AIR):
 					grid[x + position.x][y - position.y] = ENTITY_TYPES.ALLOWED					
 		"SNAKE":
+			#Snake works just like Deer
 			var new_allowed = [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(3, 1)]
+			#Except we have FORBIDDEN tiles, which we also have to mark in the grid
 			var new_forbidden = [Vector2i(4, 1), Vector2i(5, 1), Vector2i(5, 0), Vector2i(5, -1), Vector2i(4, -1)]
 			for delta in range(5):
 				grid[x + delta][y] = ENTITY_TYPES.ANIMAL
@@ -87,16 +115,21 @@ func update_grid(pos, data):
 				if(grid[x + position.x][y - position.y] == ENTITY_TYPES.AIR):
 					grid[x + position.x][y - position.y] = ENTITY_TYPES.FORBIDDEN
 		"SPIDER":
+			#Spider is the easiest, nothing much happens here
 			var new_allowed = [Vector2i(0, 1), Vector2i(1, 0), Vector2i(0, -1), Vector2i(-1, -1)]
 			grid[x][y] = ENTITY_TYPES.ANIMAL
 			set_cell(0, Vector2i(x, y), 9, Vector2i(0, 0))
 			for position in new_allowed:
 				if(grid[x + position.x][y - position.y] == ENTITY_TYPES.AIR):
 					grid[x + position.x][y - position.y] = ENTITY_TYPES.ALLOWED
+	#Whenever we change the grid, i.e. update it, we have to track that here
 	state += 1
+	#This allows us to track the previous states and return to them
 	last_states[state % save_states] = grid.duplicate(true)
 
 func make_visible():
+	#We do not color the FORBIDDEN or ALLOWED cells on ready
+	#Instead we only color them we are dragging, this function does that
 	for x in range(grid_size.x):
 		for y in range(grid_size.y):
 			if(grid[x][y] == ENTITY_TYPES.FORBIDDEN):
@@ -105,30 +138,40 @@ func make_visible():
 				set_cell(0, Vector2i(x, y), 2, Vector2i(1, 1))
 
 func make_invisible():
+	#Similarly when we are done dragging we want to return the FORBIDDEN and ALLOWED cells invisible
 	for x in range(grid_size.x):
 		for y in range(grid_size.y):
 			if(grid[x][y] == ENTITY_TYPES.FORBIDDEN or grid[x][y] == ENTITY_TYPES.ALLOWED):
 				set_cell(0, Vector2i(x, y), 0, Vector2i(1, 1))
 
 func reset_grid():
+	#To reset the grid we simple return it to the state we saved in the begining
 	grid = start_grid.duplicate(true)
+	#Then we recolor it
 	color_grid()
+	#And clean the save-states
 	last_states = []
 	for i in range(save_states):
 		last_states.append([[]])
 	state = 0
 
 func last_state():
+	#To return to the previous state of the grid we have to make sure that such a state exists
+	#Here we check if somebody tried to reset the start, this is not allowed
 	if(state == 0):
 		reset_grid()
 	else:
+		#Similarly the previous state might be empty for various rare reasons
+		#We catch that here, but it is not very necessary
 		if(last_states[state % save_states] == [[]]):
 			reset_grid()
 		else:
+			#Lastly we check if the previous state exists
 			state -= 1
 			if(last_states[state % save_states] == [[]]):
 				state += 1
 				return
+			#If it does we return the grid to it and recolor it
 			grid = last_states[state % save_states].duplicate(true)
 			last_states[(state + 1) % save_states] = [[]]
 			color_grid()
@@ -137,6 +180,8 @@ func last_state():
 func _process(delta):
 	pass
 
+
+#Down here we handle all the signal. There will be many, but most of them don't do much.
 func _on_drag_grid_need_grid():
 	current_grid.emit(grid)
 
--- Arch-enemies/bridges/script/grid_drag.gd
+++ Arch-enemies/bridges/script/grid_drag.gd
@@ -1,7 +1,10 @@
 extends TextureRect
 
+#Here we define the elements we need to operate on the grid
 var grid = []
 enum ENTITY_TYPES {GROUND, WATER, ANIMAL, FORBIDDEN, ALLOWED, AIR}
+
+#And the signal we need to communicate with the grid
 signal need_grid
 signal update_grid(pos, data)
 signal is_dragging
@@ -16,17 +19,29 @@ func _process(delta):
 	pass
 
 func _get_drag_data(at_position):
+	#When we try to drag something we first need to know what the grid looks like
 	need_grid.emit()
+	#Then we tell the grid that we are currently dragging something
+	#This allows us to see FORBIDDEN and ALLOWED zones
 	is_dragging.emit()
 	
+	#We create the data we want to transmit
 	var data = {}
+	#And the preview that is shown as we move the cursor
 	var preview = TextureRect.new()
+	
+	#These are the two things we save in the data:
+	#1) The texture of the TRect
 	var sprite = texture
+	#2) And the Tooltip, which identifies the animal
 	var animal = tooltip_text 
 	
 	if(sprite != null):
+		#If we are not trying to drag the TRect that represent the grid we create the preview
 		preview.expand = true
 		preview.texture = sprite
+		#The actualy size of the sprite depends on the animal, this is what the "animal" var is used
+		#for, to identify the animal.
 		match animal:
 			"DEER":
 				preview.set_size(Vector2(40, 40))
@@ -37,12 +52,13 @@ func _get_drag_data(at_position):
 	
 	data["sprite"] = sprite
 	data["animal"] = animal
-	
 	set_drag_preview(preview)
-	
 	return data
 
 func _can_drop_data(at_position, data):
+	#To check if we can drop we need to check the grid
+	#The Mouse is on a pixel base, while the grid has 10x10 cells
+	#Therefore we convert
 	var position = get_global_mouse_position()
 	var y = int(position.y/10)
 	var x = int(position.x/10)
@@ -50,6 +66,7 @@ func _can_drop_data(at_position, data):
 	var is_allowed = false
 	var animal = data["animal"]
 	
+	#Here we check for a given animal with its own sub-function if the grid cell is legal
 	match animal:
 		"DEER":
 			is_allowed = is_deer_allowed(pos)
@@ -61,6 +78,8 @@ func _can_drop_data(at_position, data):
 	return is_allowed
 
 func _drop_data(at_position, data):
+	#The dropping itself is simple
+	#We just return the position to the grid and update it
 	var position = Vector2(int(get_global_mouse_position().x / 10), 
 	int(get_global_mouse_position().y / 10))
 	dragging_done.emit()
@@ -71,11 +90,15 @@ func _on_grid_current_grid(current_grid):
 
 func is_snake_allowed(pos):
 	var is_allowed = false
+	#For a cell to be legal we work from the bottom left corner of the animal
+	#And check each square of the grid cell that would in the sprite
 	if(grid[pos.x][pos.y] == ENTITY_TYPES.ALLOWED):
 		var is_free = true
 		for delta in range(1, 5):
+			#If it is AIR (i.e. empty) or ALLOWED we are good
 			if(not (grid[pos.x + delta][pos.y] == ENTITY_TYPES.AIR or 
 			grid[pos.x + delta][pos.y] == ENTITY_TYPES.ALLOWED)):
+				#Otherwise we are not
 				is_free = false
 				break
 		if(is_free):
@@ -83,6 +106,7 @@ func is_snake_allowed(pos):
 	return is_allowed
 
 func is_spider_allowed(pos):
+	#This works just like Snake
 	var is_allowed = false
 	if(grid[pos.x][pos.y] == ENTITY_TYPES.ALLOWED):
 		is_allowed = true
@@ -90,6 +114,7 @@ func is_spider_allowed(pos):
 
 func is_deer_allowed(pos):
 	var is_allowed = false
+	#The Deer has empty slots in it sprite, it is allowed for them to have something in them
 	var allowed = [Vector2i(0, 3), Vector2i(3, 0), Vector2i(3, 1), Vector2i(3, 2)]
 	if(grid[pos.x][pos.y] == ENTITY_TYPES.ALLOWED):
 		var is_free = true
@@ -97,6 +122,7 @@ func is_deer_allowed(pos):
 			for epsilon in range(4):
 				if(not (grid[pos.x + delta][pos.y - epsilon] == ENTITY_TYPES.AIR or 
 				grid[pos.x + delta][pos.y - epsilon] == ENTITY_TYPES.ALLOWED)):
+					#Therefore we only flag the squares which are not in allowed
 					if(Vector2i(delta, epsilon) not in allowed):
 						is_free = false
 						break
