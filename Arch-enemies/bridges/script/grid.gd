895e650c1d839800787af9b121daf8d6c42befbd
Marcel Schoch <marcel.schoch@student.uni-tuebingen.de>
2024-01-07 14:43:14 +0100

    unify danger scenes

--- Arch-enemies/bridges/script/grid.gd
+++ Arch-enemies/bridges/script/grid.gd
@@ -124,10 +124,9 @@ func _ready():
 	# assign forbidden zones around the fox' starting position
 	grid = assign_fox_forbidden_zones(grid)
 	
-	var hazard_area = preload("res://bridges/scenes/hazard_detection.tscn")
-	var water_area = preload("res://bridges/scenes/water_detection.tscn")
-	spawn_danger_area2D(hazard_area, hazard_squares)
-	spawn_danger_area2D(water_area, water_squares)
+	var danger_area = preload("res://bridges/scenes/danger_detection.tscn")
+	var danger_squares = water_squares + hazard_squares
+	spawn_danger_area2D(danger_area, danger_squares)
 	
 	color_grid()
 	#Now we save the inital state of the grid for reset and previous state
