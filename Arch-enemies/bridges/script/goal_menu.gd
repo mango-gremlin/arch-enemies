62e7c805521d62250f1de44024e65133cf12a863
Katja Bochniak <57258671+Kabooni@users.noreply.github.com>
2024-01-16 23:45:17 +0100

    Update inventory and bridge edge after level solved

--- Arch-enemies/bridges/script/goal_menu.gd
+++ Arch-enemies/bridges/script/goal_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+signal level_solved
+
 func _on_retry_button_pressed():
 	get_parent().get_node("Player").reset_player()
 	Global.drag_mode = true
@@ -8,6 +10,19 @@ func _on_retry_button_pressed():
 
 
 func _on_continue_button_pressed():
-	# TODO return to overworld scene
+	# Update global inventory
+	level_solved.emit()
+	# Add bridge edge to Singleton, I used the values from Singleton.bridge_level_scenes
+	print("Build bridges for finish: ", SingletonPlayer.bridges_built)
+	SingletonPlayer.add_bridge_connection(get_bridge_edge())
+	print("Build bridges after finish: ", SingletonPlayer.bridges_built)
 	get_tree().change_scene_to_file("res://overworld/main_scene_overworld.tscn")
 	print("lets go to the overworld now")
+
+func get_bridge_edge():
+	var levels = SingletonPlayer.bridge_level_scenes
+	for level in levels:
+		var path = levels[level]
+		if path == get_tree().current_scene.scene_file_path:
+			return levels.find_key(path)
+		# TODO: create new entry?
