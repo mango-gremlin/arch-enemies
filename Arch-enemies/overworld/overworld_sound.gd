778ace2c66103eca97468e0aaadd9d009a42d5e9
Marcel Schoch <marcel.schoch@student.uni-tuebingen.de>
2024-01-19 02:34:21 +0100

    Sounds for quest accepted/done

--- Arch-enemies/overworld/overworld_sound.gd
+++ Arch-enemies/overworld/overworld_sound.gd
@@ -8,36 +8,28 @@ var step = preload("res://assets/sound/Step.wav")
 var quest_accepted = preload("res://assets/sound/New_Quest.wav")
 var quest_done = preload("res://assets/sound/Quest_Done.wav")
 
+func _ready():
+	SingletonPlayer.play_sound.connect(play_sound)
+
 func play_sound(sound):
+	print("entered play sound ", sound, " for overworld")
 	match sound:
 		"DEER":
 			stream = deer
-			playing = true
 		"SNAKE":
 			stream = snake
-			playing = true
 		"SPIDER":
 			stream = spider
-			playing = true
 		"SQUIRREL":
 			stream = squirrel
-			playing = true
 		"STEP":
 			stream = step
-			playing = true
 		"QUEST_ACCEPTED":
 			stream = quest_accepted
-			playing = true
-		"QUEST DONE":
+		"QUEST_DONE":
 			stream = quest_done
-			playing = true
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+	playing = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-# TODO add connections here
