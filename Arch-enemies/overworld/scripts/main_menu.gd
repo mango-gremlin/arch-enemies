349e6a3d65fc07ce72afd12f77c71743918ba3a1
Marcel Schoch <marcel.schoch@student.uni-tuebingen.de>
2024-01-31 15:05:57 +0100

    added button sounds to main menu, cutscene, about page

--- Arch-enemies/overworld/scripts/main_menu.gd
+++ Arch-enemies/overworld/scripts/main_menu.gd
@@ -1,21 +1,23 @@
 extends Control
 
+signal play_sound(sound_type)
 
-func _on_button_pressed():
-	pass # Replace with function body.
-
-
+# open settings menu as new scene
 func _on_options_pressed():
+	play_sound.emit("CLICK")
 	get_tree().change_scene_to_file("res://overworld/ui/menu/menu/settings_menu.tscn")
 
-
-func _on_exit_pressed(): #quit
+# quit game entirely
+func _on_exit_pressed():
+	play_sound.emit("CLICK")
 	get_tree().quit()
 
-
-func _on_about_pressed(): #about
+# open about page as new scene
+func _on_about_pressed():
+	play_sound.emit("CLICK")
 	get_tree().change_scene_to_file("res://overworld/ui/main_menu/about.tscn")
 
-
+# open overworld as new scene
 func _on_overworld_pressed():
+	play_sound.emit("CLICK")
 	get_tree().change_scene_to_file("res://overworld/starting_cutscene.tscn")
