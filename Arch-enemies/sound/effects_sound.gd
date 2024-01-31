c593fedfbcc47bdda9c2be18d2f413f1e75da124
Marcel Schoch <marcel.schoch@student.uni-tuebingen.de>
2024-01-31 14:09:28 +0100

    refactor to use single script for effects

--- Arch-enemies/sound/effects_sound.gd
+++ Arch-enemies/sound/effects_sound.gd
@@ -1,12 +1,18 @@
 extends AudioStreamPlayer
 #First we need to load all the sounds we intend to use
 var squirrel = preload("res://assets/sound/Squirrel.wav")
-var death = preload("res://assets/sound/Death.wav")
-var drop = preload("res://assets/sound/Drop.wav")
 var deer = preload("res://assets/sound/Deer.wav")
-var jump = preload("res://assets/sound/Jump.wav")
 var spider = preload("res://assets/sound/Spider.wav")
 var snake = preload("res://assets/sound/Snake.wav")
+
+var death = preload("res://assets/sound/Death.wav")
+var drop = preload("res://assets/sound/Drop.wav")
+var jump = preload("res://assets/sound/Jump.wav")
+var click = preload("res://assets/sound/Button.wav")
+
+var step = preload("res://assets/sound/Step.wav")
+var quest_accepted = preload("res://assets/sound/New_Quest.wav")
+var quest_done = preload("res://assets/sound/Quest_Done.wav")
 
 func play_sound(sound:String) -> void:
 	#Here we match the sound we are supposed to be playing
@@ -25,12 +31,17 @@ func play_sound(sound:String) -> void:
 			stream = spider
 		"SQUIRREL":
 			stream = squirrel
+		"QUEST_ACCEPTED":
+			stream = quest_accepted
+		"QUEST_DONE":
+			stream = quest_done
 	#Then we play it
 	playing = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.volume_db = Settings.get_effects_volume()
+	SingletonPlayer.play_sound.connect(play_sound)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
