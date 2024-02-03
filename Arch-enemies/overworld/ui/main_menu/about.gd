e0c657f7e1efb24a613b3dc5e89d12d34cc5a872
mango-gremlin <mara.cockburn@student.uni-tuebingen.de>
2024-02-03 11:58:00 +0100

    added names, links to fonts and github and licensing information

--- Arch-enemies/overworld/ui/main_menu/about.gd
+++ Arch-enemies/overworld/ui/main_menu/about.gd
@@ -5,3 +5,8 @@ signal play_sound(sound_type)
 func _on_button_pressed():
 	play_sound.emit("CLICK")
 	get_tree().change_scene_to_file("res://overworld/ui/main_menu/main_menu.tscn")
+
+
+func _on_licensing_pressed():
+	play_sound.emit("CLICK")
+	get_tree().change_scene_to_file("res://overworld/ui/main_menu/licensing.tscn")
