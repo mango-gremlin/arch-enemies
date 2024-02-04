269bb3f2a6c48065614d21170b09e06964165100
mango-gremlin <mara.cockburn@student.uni-tuebingen.de>
2024-02-04 11:07:33 +0100

    added sound to the link buttons and removed invisible frog

--- Arch-enemies/overworld/ui/main_menu/about.gd
+++ Arch-enemies/overworld/ui/main_menu/about.gd
@@ -10,3 +10,12 @@ func _on_button_pressed():
 func _on_licensing_pressed():
 	play_sound.emit("CLICK")
 	get_tree().change_scene_to_file("res://overworld/ui/main_menu/licensing.tscn")
+
+
+func _on_link_button_pressed():
+	play_sound.emit("CLICK")
+	
+
+
+func _on_link_button_2_pressed():
+	play_sound.emit("CLICK")
