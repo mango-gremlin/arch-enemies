38553d1d94ae969f9a384d842a6cce1029898f54
Marcel Schoch <marcel.schoch@student.uni-tuebingen.de>
2024-02-12 14:47:12 +0100

    play sound before transitioning to new scene

--- Arch-enemies/bridges/menus/goal_menu.gd
+++ Arch-enemies/bridges/menus/goal_menu.gd
@@ -23,8 +23,8 @@ func _on_continue_button_pressed():
 	var current_edge = SingletonPlayer.get_current_bridge_edge()
 	SingletonPlayer.add_bridge_connection(current_edge)
 	#SingletonPlayer.set_current_bridge_edge(null)
-	get_tree().change_scene_to_file("res://overworld/main_scene_overworld.tscn")
 	play_sound.emit("CLICK")
+	get_tree().change_scene_to_file("res://overworld/main_scene_overworld.tscn")
 
 func _on_back_button_pressed():
 	#reset everthing except the placed animals
